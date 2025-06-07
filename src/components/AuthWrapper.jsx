80f85b15b244efa2ee48ab2b73a4269e08fae6c4
THEkingmay <beerranee@gmail.com>
2025-06-07 16:50:23 +0700

    get data from firestore in Context only

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -6,33 +6,51 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { auth } from '../data/UserAuth'
 import Loading from '../items/Loading'
 
-const AuthContext = createContext()
+const AuthContext = createContext()   // รวมทุกอย่างไว้ที่ context เพื่อประหยัดโควต้า firestore
 
 export default function AuthWrapper({ children }) {
   const [currentUserID, setUID] = useState(null)
   const [loading, setLoading] = useState(true)
-
+  const [studyPlanData , setPlanData ] = useState([]) // แผนการเรียนยังไงก็ต้องมีอันเดียวฉะนั้นใช้ index 0 เสมอ !!
 
   const navigate = useNavigate()
   const location = useLocation()
 
+  const fetchUersStudyplan = async () =>{
+          try{
+              const data = await getUserStudyplan(currentUserID)
+              const tmp=[]
+              data.forEach(d=>tmp.push({id:d.id , data: d.data()}))            
+              setPlanData(tmp)
+          }catch(err){
+              console.log(err.message)
+          }
+      }
+
   useEffect(() => {
     setLoading(true)
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      const id = user ? user.uid : null
-      setUID(id)
-
+      if(user){
+        setUID(user.uid)
+      }else{
+        setUID(null)
+        setPlanData([])
+      }
       if (!user && location.pathname !== '/loginRegister') {
         navigate('/loginRegister')
       } else if (user && location.pathname === '/loginRegister') {
         navigate('/dashboard')
       }
-
       setLoading(false)
     })
-
     return () => unsubscribe()
   }, [navigate, location.pathname])
+
+  useEffect(() => {
+    if (currentUserID) {
+      fetchUersStudyplan()
+    }
+  }, [currentUserID])
 
   if (loading) {
     return(
@@ -40,7 +58,7 @@ export default function AuthWrapper({ children }) {
     }
 
   return (
-    <AuthContext.Provider value={{ currentUserID , navigate }}>
+    <AuthContext.Provider value={{ currentUserID , navigate , studyPlanData , fetchUersStudyplan}}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,20 @@
-import { useEffect , useState} from "react"
-import { getUserStudyplan } from "../data/fireStore"
-import Loading from "../items/Loading"
 import { useContext } from "react"
 import { AuthContext } from "../components/AuthWrapper"
 
 export default function Dashbord(){
 
-    const [isLoad , setLoad ] = useState(false)
-    const {currentUserID , navigate} = useContext(AuthContext)
-    const [studyPlanData , setPlanData ] = useState([])
-
-
-    const fetchUersStudyplan = async () =>{
-        try{
-            setLoad(true)
-            const data = await getUserStudyplan(currentUserID)
-            const tmp=[]
-            data.forEach(d=>tmp.push({id:d.id , data: d.data()}))            
-            setPlanData(tmp)
-        }catch(err){
-            console.log(err.message)
-        }finally{
-            setLoad(false)
-        }
-    }
-
-    useEffect(()=>{
-     fetchUersStudyplan()
-    },[])
+    const {navigate , studyPlanData} = useContext(AuthContext)
 
     return(
         <div className="container p-4 ">
-            {!isLoad && 
             <div>
                 <div className="display-3 text-center">Dashboard</div>
-
+                { studyPlanData.length===0 && (
+                        <div>
+                            You need to add your studyplan fisrt
+                        </div>
+                )}
             </div>
-            }
-            <Loading status={isLoad}/>
         </div>
     )
 }
